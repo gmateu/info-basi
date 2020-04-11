35f657bafff06ade09885ed253459a7a8e848484
Your Name <you@example.com>
2020-04-11 06:43:46 +0200

    dissabte sant

--- header.php
+++ header.php
@@ -1,3 +1,17 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+?>
+
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
