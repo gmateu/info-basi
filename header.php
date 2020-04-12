fd041ff14b022578725c4897063fdb2cd276f2ac
Your Name <you@example.com>
2020-04-12 18:08:12 +0200

    hem fet responsive la capçalera

--- header.php
+++ header.php
@@ -31,36 +31,34 @@
     <section id="top-bar">
         <div class="container">
             <div class="row">
-                <div class="brand col-3">logo</div>
-                <div class="second-column col-9">
-                    <div class="account">cuenta</div>
-                    <nav class="main-menu navbar navbar-expand-md navbar-light" role="navigation">
-                            <!-- Brand and toggle get grouped for better mobile display -->
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                                <?php
-                                wp_nav_menu( array(
-                                    'theme_location'    => 'info_basic_main_menu',
-                                    'depth'             => 3,
-                                    'container'         => 'div',
-                                    'container_class'   => 'collapse navbar-collapse',
-                                    'container_id'      => 'bs-example-navbar-collapse-1',
-                                    'menu_class'        => 'nav navbar-nav',
-                                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-                                    'walker'            => new WP_Bootstrap_Navwalker(),
-                                ) );
-                                ?>
-                    </nav>
+                <div class="brand col-12 col-md-3 col-lg-2 text-center text-md-left">logo</div>
+                <div class="second-column col-12 col-md-9 col-lg-10">
+                    <div class="row">
+                        <div class="account col-12">cuenta</div>
+                        <div class="col-12">
+                            <nav class="main-menu navbar navbar-expand-md navbar-light" role="navigation">
+                                    <!-- Brand and toggle get grouped for better mobile display -->
+                                    <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span class="navbar-toggler-icon"></span>
+                                    </button>
+                                        <?php
+                                        wp_nav_menu( array(
+                                            'theme_location'    => 'info_basic_main_menu',
+                                            'depth'             => 3,
+                                            'container'         => 'div',
+                                            'container_class'   => 'collapse navbar-collapse',
+                                            'container_id'      => 'bs-example-navbar-collapse-1',
+                                            'menu_class'        => 'nav navbar-nav',
+                                            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                                            'walker'            => new WP_Bootstrap_Navwalker(),
+                                        ) );
+                                        ?>
+                            </nav>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
 </header>
-                        <?php 
-                            wp_nav_menu(
-                                array(
-                                    'theme_location' => 'info_basic_main_menu'
-                                )
-                            );
-                        ?>
+                        
